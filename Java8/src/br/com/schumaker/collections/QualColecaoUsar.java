a115fdffb0ddb24539df21df32649d27caf2924d
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-08-06 12:50:13 +0200

    Update QualColecaoUsar.java

--- Java8/src/br/com/schumaker/collections/QualColecaoUsar.java
+++ Java8/src/br/com/schumaker/collections/QualColecaoUsar.java
@@ -3,6 +3,9 @@ package br.com.schumaker.collections;
 import java.util.Collection;
 import java.util.HashSet;
 
+/**
+ * @author Hudson Schumaker
+ */
 public class QualColecaoUsar {
 
     public static void main(String[] args) {
